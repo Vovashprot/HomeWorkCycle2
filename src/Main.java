32a2a19f5522f1547d6bf0f448f4fd972cdd8a2f
Vovashprot <vova.protasenko@gmail.com>
2023-02-21 14:35:34 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static  void task1(){
+        System.out.println("Task 1");
         int deposit = 0;
         int month = 0;
         while(deposit < 2459000){
@@ -14,6 +16,7 @@ public class Main {
         }
     }
     public static  void task2(){
+        System.out.println("Task 2");
         int i = 0;
         while(i <= 10){
             System.out.print(i+" ");
@@ -24,5 +27,18 @@ public class Main {
             System.out.print(i+" ");
         }
 
+    }
+    public static  void task3(){
+        System.out.println("Task 3");
+        int people = 12000000;
+        int year = 1;
+        int deathsInYear = people/1000*8;
+        int birthsInYear = people/1000*17;
+        while(year <= 10){
+            people = people + birthsInYear - deathsInYear;
+            System.out.println("Год "+ year +", численность населения составляет " + people);
+            year = year + 1;
+
+        }
     }
 }
