136f7964286b4f1329b24b1ebf0db2273e118d56
Vovashprot <vova.protasenko@gmail.com>
2023-02-24 22:47:36 +0300

    Task4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import com.sun.jdi.VMOutOfMemoryException;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
     }
     public static  void task1(){
         System.out.println("Task 1");
@@ -38,6 +41,19 @@ public class Main {
             people = people + birthsInYear - deathsInYear;
             System.out.println("Год "+ year +", численность населения составляет " + people);
             year = year + 1;
+
+        }
+    }
+    public static  void task4(){
+        System.out.println("Task 4");
+        int deposit = 15000;
+        int monthPlus = deposit/100*7;
+        int month = 0;
+        while(deposit < 12000000){
+            deposit = deposit + monthPlus;
+            monthPlus = deposit/100*7;
+            month = month + 1;
+            System.out.println("Денег: "+ deposit +" ; месяц" + month);
 
         }
     }
